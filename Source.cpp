bf11666654eb39da695add80b396f07ac6831336
Thanh <lachithanh0011@gmail.com>
2025-10-27 08:21:02 +0700

    Tich Thuong

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,24 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
+int tich(int a, int b)
+{
+	return a*b;
+}
+float thuong(int a, int b)
+{
+	if(b==0)
+		return 0;
+	else
+		return a/(1.0)*b;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a=2;
+	int b=6;
+	cout<<tich(a,b);
+	cout<<thuont(a,b);
 	system("pause");
 	return 0;
 }
